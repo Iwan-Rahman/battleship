168c617b7c80989cad334ffff2ba07d42d5bc098
Iwan Rahman <1w3n.rahman@gmail.com>
2024-01-06 21:53:11 -0500

    Create ship object

---
+++ src/ship.js
@@ -0,0 +1,10 @@
+const ship = (length) => {
+  let hits = 0
+  let sunk = false
+
+  const hit = () => ++hits;
+  const isSunk = () => sunk = hits == length ? true : false;
+  return {hit,isSunk}
+};
+
+export default ship;
